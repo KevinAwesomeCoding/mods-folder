3871334348150737abe4f079b7286f10847d21ca
KevinAwesomeCoding <70450869+KevinAwesomeCoding@users.noreply.github.com>
2026-01-11 16:20:24 -0600

    Add category selection for modpack installation
    Refactor modpack installer to support categories and improve UI.

--- installer.py
+++ installer.py
@@ -10,127 +10,145 @@ import sys
 import platform
 
 # --- MODPACKS CONFIGURATION ---
-# Note: I added a 'loader_url' field to each pack for the version download links.
+# Structure: "Category Name": { "Modpack Name": { details... } }
 MODPACKS = {
-    "Wonderland": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/wonderland/mods.zip",
-        "profile_name": "Wonderland",
-        "folder_name": "Wonderland",
-        "version_id": "1.20.1-forge-47.4.10",
-        "icon": "Furnace",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
+    "Horror": {
+        "Wonderland": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/wonderland/mods.zip",
+            "profile_name": "Wonderland",
+            "folder_name": "Wonderland",
+            "version_id": "1.20.1-forge-47.4.10",
+            "icon": "Furnace",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
+        },
+        "The Backrooms": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/backrooms/mods.zip",
+            "profile_name": "Backrooms",
+            "folder_name": "Backrooms",
+            "version_id": "fabric-loader-0.18.4-1.20.1",
+            "icon": "Bookshelf",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/fabric-loader-0.18.4-1.20.1.zip"
+        },
+        "The Anomaly": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/anomaly/mods.zip",
+            "profile_name": "The Anomaly",
+            "folder_name": "The Anomaly",
+            "version_id": "1.20.1-forge-47.4.10",
+            "icon": "Obsidian",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
+        },
+        "The One Who Watches": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/onewhowatches/mods.zip",
+            "profile_name": "The One Who Watches",
+            "folder_name": "The One Who Watches",
+            "version_id": "1.20.1-forge-47.4.10",
+            "icon": "Carved_Pumpkin",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
+        },
+        "The Obsessed": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/obsessed/mods.zip",
+            "profile_name": "The Obsessed",
+            "folder_name": "The Obsessed",
+            "version_id": "1.20.1-forge-47.4.10",
+            "icon": "Netherrack",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
+        },
+        "From The Fog": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/fromthefog/mods.zip",
+            "profile_name": "From The Fog",
+            "folder_name": "From The Fog",
+            "version_id": "fabric-loader-0.18.4-1.21.11",
+            "icon": "Glass",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/fabric-loader-0.18.4-1.21.11.zip"
+        },
+        "The Broken Script": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/brokenscript/mods.zip",
+            "profile_name": "The Broken Script",
+            "folder_name": "The Broken Script",
+            "version_id": "1.20.1-forge-47.4.10",
+            "icon": "Redstone_Block",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
+        },
+        "000.jar": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/000/mods.zip",
+            "profile_name": "000.jar",
+            "folder_name": "000.jar",
+            "version_id": "1.19.2-forge-43.5.2",
+            "icon": "Barrier",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.19.2-forge-43.5.2.zip"
+        },
+        "The Newest Goatman": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/goatman/mods.zip",
+            "profile_name": "The Newest Goatman",
+            "folder_name": "The Newest Goatman",
+            "version_id": "1.19.2-forge-43.5.2",
+            "icon": "Bone_Block",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.19.2-forge-43.5.2.zip"
+        },
+        "Sanity": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/sanity/mods.zip",
+            "profile_name": "Sanity",
+            "folder_name": "Sanity",
+            "version_id": "1.20.1-forge-47.4.10",
+            "icon": "Soul_Sand",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
+        },
+        "The God": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/thegod/mods.zip",
+            "profile_name": "The God",
+            "folder_name": "The God",
+            "version_id": "1.20.1-forge-47.4.10",
+            "icon": "Beacon",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
+        },
+        "From The Caves": {
+            "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/fromthecaves/mods.zip",
+            "profile_name": "From The Caves",
+            "folder_name": "From The Caves",
+            "version_id": "1.20.1-forge-47.4.10",
+            "icon": "Bedrock",
+            "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
+        }
     },
-    "The Backrooms": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/backrooms/mods.zip",
-        "profile_name": "Backrooms",
-        "folder_name": "Backrooms",
-        "version_id": "fabric-loader-0.18.4-1.20.1",
-        "icon": "Bookshelf",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/fabric-loader-0.18.4-1.20.1.zip"
-    },
-    "The Anomaly": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/anomaly/mods.zip",
-        "profile_name": "The Anomaly",
-        "folder_name": "The Anomaly",
-        "version_id": "1.20.1-forge-47.4.10",
-        "icon": "Obsidian",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
-    },
-    "The One Who Watches": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/onewhowatches/mods.zip",
-        "profile_name": "The One Who Watches",
-        "folder_name": "The One Who Watches",
-        "version_id": "1.19.2-forge-43.5.2",
-        "icon": "Carved_Pumpkin",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.19.2-forge-43.5.2.zip"
-    },
-    "The Obsessed": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/obsessed/mods.zip",
-        "profile_name": "The Obsessed",
-        "folder_name": "The Obsessed",
-        "version_id": "1.20.1-forge-47.4.10",
-        "icon": "Netherrack",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
-    },
-    "From The Fog": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/fromthefog/mods.zip",
-        "profile_name": "From The Fog",
-        "folder_name": "From The Fog",
-        "version_id": "fabric-loader-0.18.4-1.21.11",
-        "icon": "Glass",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/fabric-loader-0.18.4-1.21.11.zip"
-    },
-    "The Broken Script": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/brokenscript/mods.zip",
-        "profile_name": "The Broken Script",
-        "folder_name": "The Broken Script",
-        "version_id": "1.20.1-forge-47.4.10",
-        "icon": "Redstone_Block",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
-    },
-    "000.jar": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/000/mods.zip",
-        "profile_name": "000.jar",
-        "folder_name": "000.jar",
-        "version_id": "1.19.2-forge-43.5.2",
-        "icon": "Barrier",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.19.2-forge-43.5.2.zip"
-    },
-    "The Newest Goatman": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/goatman/mods.zip",
-        "profile_name": "The Newest Goatman",
-        "folder_name": "The Newest Goatman",
-        "version_id": "1.19.2-forge-43.5.2",
-        "icon": "Bone_Block",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.19.2-forge-43.5.2.zip"
-    },
-    "Sanity": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/sanity/mods.zip",
-        "profile_name": "Sanity",
-        "folder_name": "Sanity",
-        "version_id": "1.20.1-forge-47.4.10",
-        "icon": "Soul_Sand",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
-    },
-    "The God": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/thegod/mods.zip",
-        "profile_name": "The God",
-        "folder_name": "The God",
-        "version_id": "1.20.1-forge-47.4.10",
-        "icon": "Beacon",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
-    },
-    "From The Caves": {
-        "url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/fromthecaves/mods.zip",
-        "profile_name": "From The Caves",
-        "folder_name": "From The Caves",
-        "version_id": "1.20.1-forge-47.4.10",
-        "icon": "Deepslate",
-        "loader_url": "https://github.com/KevinAwesomeCoding/mods-folder/releases/download/versions/1.20.1-forge-47.4.10.zip"
-    },
+    "Challenge": {
+        # --- ADD YOUR CHALLENGE MODS HERE LATER ---
+        # Example format:
+        # "One Block": { ... details ... },
+    }
 }
 
 class InstallerApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Modpack Installer")
-        self.root.geometry("400x400")
+        self.root.geometry("400x450") # Made slightly taller for extra dropdown
         
         # Header
-        tk.Label(root, text="Select a Modpack", font=("Segoe UI", 16, "bold")).pack(pady=20)
+        tk.Label(root, text="Select a Modpack", font=("Segoe UI", 16, "bold")).pack(pady=15)
         
-        # Dropdown
-        tk.Label(root, text="Choose which server to install:", font=("Segoe UI", 10)).pack(pady=(0, 5))
+        # --- CATEGORY DROPDOWN ---
+        tk.Label(root, text="Select Category:", font=("Segoe UI", 10, "bold")).pack(pady=(5, 0))
+        self.selected_category = tk.StringVar()
+        self.cat_dropdown = ttk.Combobox(root, textvariable=self.selected_category, state="readonly", font=("Segoe UI", 10))
+        self.cat_dropdown['values'] = list(MODPACKS.keys())
+        self.cat_dropdown.bind("<<ComboboxSelected>>", self.update_pack_dropdown) # When category changes, update packs
+        self.cat_dropdown.current(0) # Select first category by default
+        self.cat_dropdown.pack(pady=5, ipadx=20)
+
+        # --- MODPACK DROPDOWN ---
+        tk.Label(root, text="Select Modpack:", font=("Segoe UI", 10)).pack(pady=(10, 0))
         self.selected_pack = tk.StringVar()
-        self.dropdown = ttk.Combobox(root, textvariable=self.selected_pack, state="readonly", font=("Segoe UI", 10))
-        self.dropdown['values'] = list(MODPACKS.keys())
-        self.dropdown.current(0)
-        self.dropdown.pack(pady=10, ipadx=20)
+        self.pack_dropdown = ttk.Combobox(root, textvariable=self.selected_pack, state="readonly", font=("Segoe UI", 10))
+        # Values will be filled by update_pack_dropdown
+        self.pack_dropdown.pack(pady=5, ipadx=20)
         
+        # Initialize the second dropdown based on the first one
+        self.update_pack_dropdown(None)
+
         # Install Button
         self.btn_install = tk.Button(root, text="Install Selected Pack", command=self.start_thread, 
                                      bg="#4CAF50", fg="white", font=("Segoe UI", 12, "bold"), height=2, width=20)
-        self.btn_install.pack(pady=20)
+        self.btn_install.pack(pady=25)
         
         # Progress Bar (Hidden by default)
         self.progress_var = tk.DoubleVar()
@@ -140,16 +158,35 @@ class InstallerApp:
         self.status = tk.Label(root, text="Ready", fg="gray")
         self.status.pack(side="bottom", pady=10)
 
+    def update_pack_dropdown(self, event):
+        """Updates the Modpack dropdown based on the selected Category."""
+        category = self.selected_category.get()
+        packs = list(MODPACKS[category].keys())
+        
+        self.pack_dropdown['values'] = packs
+        if packs:
+            self.pack_dropdown.current(0)
+            self.btn_install.config(state="normal")
+        else:
+            self.pack_dropdown.set("No packs in this category")
+            self.btn_install.config(state="disabled")
+
     def start_thread(self):
-        """Starts the installation process in a separate thread."""
         self.btn_install.config(state="disabled", text="Installing...")
         self.progress_bar.pack(fill="x", padx=40, pady=10)
         threading.Thread(target=self.run_install, daemon=True).start()
 
     def run_install(self):
         try:
+            # We now need to get config using Category AND Pack Name
+            category = self.selected_category.get()
             pack_name = self.selected_pack.get()
-            config = MODPACKS[pack_name]
+            
+            # Safety check
+            if pack_name not in MODPACKS[category]:
+                raise Exception("Invalid selection.")
+
+            config = MODPACKS[category][pack_name]
             mc_dir = self.get_mc_dir()
             
             # --- STEP 1: AUTO-INSTALL LOADER IF MISSING ---
@@ -167,57 +204,57 @@ class InstallerApp:
             self.root.after(0, lambda: messagebox.showinfo("Success", f"Installed '{pack_name}' successfully!"))
             
         except Exception as e:
-            # Error handling
             self.root.after(0, lambda: messagebox.showerror("Error", str(e)))
             self.root.after(0, self.reset_ui)
             self.root.after(0, lambda: self.status.config(text="Error occurred."))
 
     def check_loader_installed(self, version_id):
-        """Checks if .minecraft/versions/<version_id> exists."""
         mc_dir = self.get_mc_dir()
         version_path = os.path.join(mc_dir, 'versions', version_id)
         return os.path.exists(version_path)
 
     def install_loader(self, mc_dir, loader_url):
-        """Downloads and extracts the loader zip into .minecraft/versions"""
         versions_dir = os.path.join(mc_dir, 'versions')
         if not os.path.exists(versions_dir): os.makedirs(versions_dir)
         
         temp_loader_zip = os.path.join(versions_dir, "temp_loader.zip")
-        
-        # Download
         self.progress_var.set(0)
         self.download_file(loader_url, temp_loader_zip)
         
-        # Extract directly into versions folder
         self.update_status("Installing Loader files...")
         with zipfile.ZipFile(temp_loader_zip, 'r') as z:
             z.extractall(versions_dir)
-            
         os.remove(temp_loader_zip)
 
     def install_modpack_logic(self, mc_dir, config):
-        """Main modpack installation (Mods + Profile)"""
         if not os.path.exists(mc_dir): raise Exception("Minecraft not found.")
         
-        # 1. Prepare Profile Folder
         profile_dir = os.path.join(mc_dir, 'profiles', config['folder_name'])
         if not os.path.exists(profile_dir): os.makedirs(profile_dir)
         
-        # 2. Download Mods
         self.update_status(f"Downloading {config['profile_name']} Mods...")
         self.progress_var.set(0)
         
         temp_zip = os.path.join(profile_dir, "temp.zip")
         self.download_file(config['url'], temp_zip)
         
-        # 3. Extract Mods
         self.update_status("Extracting Mods...")
         self.progress_var.set(100)
         with zipfile.ZipFile(temp_zip, 'r') as z: z.extractall(profile_dir)
         os.remove(temp_zip)
         
-        # 4. Create/Update Profile in Launcher
+        # --- NEW: MOVE VERSION FOLDER IF IT EXISTS ---
+        extracted_version_source = os.path.join(profile_dir, "version_data")
+        if os.path.exists(extracted_version_source):
+            self.update_status("Installing Loader Version...")
+            main_versions_dir = os.path.join(mc_dir, "versions")
+            for item in os.listdir(extracted_version_source):
+                s = os.path.join(extracted_version_source, item)
+                d = os.path.join(main_versions_dir, item)
+                if not os.path.exists(d):
+                    shutil.move(s, d)
+            shutil.rmtree(extracted_version_source)
+
         self.update_json_profile(mc_dir, config['profile_name'], profile_dir, config['version_id'], config['icon'])
 
     def reset_ui(self):
@@ -237,7 +274,6 @@ class InstallerApp:
             total_size = int(response.info().get('Content-Length', 0))
             block_size = 8192
             downloaded = 0
-            
             with open(path, 'wb') as out_file:
                 while True:
                     chunk = response.read(block_size)
@@ -282,5 +318,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = InstallerApp(root)
     root.mainloop()
-
-
